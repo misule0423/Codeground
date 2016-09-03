5759c49fdc73aed771b650cd6fdc39d312f176e3
misule0423 <misule0423@gmail.com>
2016-09-03 21:39:01 +0900

    Update easy_1.cpp

--- easy_1.cpp
+++ easy_1.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  CODEGROUND
@@ -6,9 +7,9 @@
 //  Copyright © 2016년 Roop. All rights reserved.
 //
 
+// 아래는 XOR 성질을 이용하였다. 어떤 수 x가 있을때 (x ^ 0) = x, (x ^ x) = 0 이 되는 성질을 이용하였다.
+// 즉, 홀수, 짝수에 상관 없이 홀수번을 입력받으면 자기 자신이 되고, 짝수번을 입력받으면 0이되므로 수를 셀 필요 조차 없다.
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -25,49 +26,85 @@ int main(int argc, const char * argv[]) {
         
         cin >> n;
         
-        long long N;
-        
-        vector<long long> v;
+        int result = 0;
+        int input = 0;
         
         for (int i=0; i<n; i++) {
-            cin >> N;
-            v.push_back(N);
+            cin >> input;
+            result ^= input;
         }
-        
-        sort(v.begin(), v.end());
-        
-        vector<long long> _xor;
-        int count=1;
-        long long k = v.back();
-        while(!v.empty()){
-            v.pop_back();
-            if(k == v.back()){
-                count++;
-            } else {
-                if(count%2 == 1){
-                    _xor.push_back(k);
-                    count = 1;
-                    k = v.back();
-                }
-                else {
-                    k = v.back();
-                    count = 1;
-                }
-            }
-        }
-        
-        long long result = 0;
-        for (int i=0; i<_xor.size(); i++) {
-            result ^= _xor[i];
-        }
-
         
         cout << "Case #" << c-t << endl << result << endl;
         
-        v.clear();
-        _xor.clear();
-        result = 0;
     }
     
     return 0;
 }
+
+//아래는 n개를 입력 받고, 정렬한 후, 하나씩 pop 하면서 그 value의 갯수를 세고, 홀수일경우 새로운 벡터에 추가하였다.
+//그리고 그것을 XOR하였다.
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main(int argc, const char * argv[]) {
+//    
+//    int t;
+//    
+//    cin >> t;
+//    
+//    int c = t;
+//    
+//    while (t--) {
+//        
+//        int n;
+//        
+//        cin >> n;
+//        
+//        long long N;
+//        
+//        vector<long long> v;
+//        
+//        for (int i=0; i<n; i++) {
+//            cin >> N;
+//            v.push_back(N);
+//        }
+//        
+//        sort(v.begin(), v.end());
+//        
+//        vector<long long> _xor;
+//        int count=1;
+//        long long k = v.back();
+//        while(!v.empty()){
+//            v.pop_back();
+//            if(k == v.back()){
+//                count++;
+//            } else {
+//                if(count%2 == 1){
+//                    _xor.push_back(k);
+//                    count = 1;
+//                    k = v.back();
+//                }
+//                else {
+//                    k = v.back();
+//                    count = 1;
+//                }
+//            }
+//        }
+//        
+//        long long result = 0;
+//        for (int i=0; i<_xor.size(); i++) {
+//            result ^= _xor[i];
+//        }
+//        
+//        
+//        cout << "Case #" << c-t << endl << result << endl;
+//        
+//        v.clear();
+//        _xor.clear();
+//        result = 0;
+//    }
+//    
+//    return 0;
+//}
